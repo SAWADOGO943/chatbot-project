82e8b9518d8c7d48d01fce2cc00a8b7135a4e7b9
SAWADOGO943 <stevenyoussoufs@gmail.com>
2026-03-27 17:45:32 +0000

    modification des autorisations

--- backend/main.py
+++ backend/main.py
@@ -35,7 +35,11 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=[
+        "http://localhost:5500",
+        "http://127.0.0.1:5500",
+        "https://chatbot-project.vercel.app/",
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
